a7e211f7a1aed4185b521175eb32286ed1277b7f
Christian Krause <christian.krause@idiv.de>
2019-06-12 14:54:23 +0200

    bumps version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name = "mailto-uri",
-    version = "1.1.0",
+    version = "1.2.0",
     author = "Christian Krause",
     author_email = "christian.krause@idiv.de",
     description = "create mailto URIs",
